5bee50caeff184a592205658f5f24a81c39c6a5a
Liangxianguang <2811306715@qq.com>
2026-08-21 09:15:38 +0800

    chore(repo): prune artifacts and document reproducibility

--- third_party/gym-pybullet-drones-7688e7208a1572b1680736a3c0c9b93c379db3fe/gym_pybullet_drones/__init__.py
+++ third_party/gym-pybullet-drones-7688e7208a1572b1680736a3c0c9b93c379db3fe/gym_pybullet_drones/__init__.py
@@ -6,57 +6,6 @@ register(
 )
 
 register(
-    id='dyn-aviary-v0',
-    entry_point='gym_pybullet_drones.envs:DynAviary',
-)
-
-register(
     id='velocity-aviary-v0',
     entry_point='gym_pybullet_drones.envs:VelocityAviary',
-)
-
-register(
-    id='vision-aviary-v0',
-    entry_point='gym_pybullet_drones.envs:VisionAviary',
-)
-
-
-
-
-register(
-    id='takeoff-aviary-v0',
-    entry_point='gym_pybullet_drones.envs.single_agent_rl:TakeoffAviary',
-)
-
-register(
-    id='hover-aviary-v0',
-    entry_point='gym_pybullet_drones.envs.single_agent_rl:HoverAviary',
-)
-
-register(
-    id='flythrugate-aviary-v0',
-    entry_point='gym_pybullet_drones.envs.single_agent_rl:FlyThruGateAviary',
-)
-
-register(
-    id='tune-aviary-v0',
-    entry_point='gym_pybullet_drones.envs.single_agent_rl:TuneAviary',
-)
-
-
-
-
-register(
-    id='flock-aviary-v0',
-    entry_point='gym_pybullet_drones.envs.multi_agent_rl:FlockAviary',
-)
-
-register(
-    id='leaderfollower-aviary-v0',
-    entry_point='gym_pybullet_drones.envs.multi_agent_rl:LeaderFollowerAviary',
-)
-
-register(
-    id='meetup-aviary-v0',
-    entry_point='gym_pybullet_drones.envs.multi_agent_rl:MeetupAviary',
 )
